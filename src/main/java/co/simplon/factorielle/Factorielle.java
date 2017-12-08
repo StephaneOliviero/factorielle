7e953bca33f1bfd847181f57ce54eeac4534ce3b
Stephane <Stephane@example.com>
2017-12-08 14:32:12 +0100

    ajout de log

--- src/main/java/co/simplon/factorielle/Factorielle.java
+++ src/main/java/co/simplon/factorielle/Factorielle.java
@@ -1,5 +1,8 @@
 package co.simplon.factorielle;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Factorielle
  */
@@ -13,6 +16,7 @@ public class Factorielle
     }
     
     public long calculer(long n) { 
+    	    System.out.println("n = " + n);
     		if (n > 1) {
     			return n * calculer(n-1);
     		} else if (n < 0) { 
@@ -22,7 +26,17 @@ public class Factorielle
     }
 
 
-
+    public static void main(String[] args) {
+    	
+    	Logger logger = LoggerFactory.getLogger(Factorielle.class);
+        logger.info("Factorielle");
+    	
+    	long entier = 50;
+		Factorielle factorielle = new Factorielle(); 
+		
+		long retour=factorielle.calculer(entier);
+    	
+    }
 
 }
 
